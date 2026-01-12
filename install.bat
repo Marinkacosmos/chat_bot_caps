4df4b797fecbd7c3cfeae4772f4fec1fe1278fb0
Marinkacosmos <163525776+Marinkacosmos@users.noreply.github.com>
2026-01-12 13:05:21 +0300

    Update install.bat
    start ollama server

--- install.bat
+++ install.bat
@@ -7,7 +7,12 @@ CALL "%USERPROFILE%\anaconda3\Scripts\conda.exe" env create -f .\environment.yml
 REM ---- 2. Initialize Conda env----
 CALL "%USERPROFILE%\anaconda3\Scripts\activate.bat" ocr-llm-bot
 
-REM ---- 3. Download Ollama model----
+REM ---- 3. Start Ollama server in background ----
+echo Starting Ollama server...
+start "" /B ollama.exe serve
+
+REM ---- 4. Download Ollama model----
 ollama pull gpt-oss
 
 endlocal
+
